7eb7f1961d6b08fabfe060d515c6e6c951c1e948
kiran <kirancr2000@gmail.com>
2021-05-27 23:57:28 +0530

    display database item

--- todolist/views.py
+++ todolist/views.py
@@ -5,4 +5,4 @@ from .models import Todolist
 def index(request):
     todo_items = Todolist.objects.order_by('id')
     context = {'todo_items': todo_items}
-    return render(request,"todolist/index.html",context)
+    return render(request,'todolist/index.html',context)
