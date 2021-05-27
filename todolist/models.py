3722dfaedd811920cd16eff315446fbd43ffff09
kiran <kirancr2000@gmail.com>
2021-05-27 22:50:23 +0530

    first commit with templetes and Model

--- todolist/models.py
+++ todolist/models.py
@@ -5,3 +5,7 @@ class Todolist(models.Model):
     text = models.CharField(max_length=45)
     completed = models.BooleanField(default=False)
     
+    
+    def __str__(self):
+        return self.text 
+    
